d6b9cf68aec455fdbf18bc272102835bf33492ea
Ana Raquel Matos <anaraquelpmatos@gmail.com>
2022-05-29 16:46:45 -0300

    add: GET /categories

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ app.use(json());
 app.use(cors());
 dotenv.config();
 
+app.get('/categories', (req, res) => {
+    try{
+        const categories = database.query("SELECT * FROM categories");
+        res.send(categories);
+    }
+    catch (e){
+        res.status(500).send(e);
+    }
+})
 
 app.listen(4000, () => {
     console.log("Servidor conectado!");
