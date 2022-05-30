bc7a514c5af2b621ba06056b291744f963d9e037
Ana Raquel Matos <anaraquelpmatos@gmail.com>
2022-05-30 02:39:21 -0300

    add: GET /games

--- index.js
+++ index.js
@@ -9,39 +9,53 @@ app.use(json());
 app.use(cors());
 dotenv.config();
 
-app.get('/categories', (req, res) => {
-    try{
-        const categories = database.query("SELECT * FROM categories");
-        res.send(categories);
+app.get('/categories', async (req, res) => {
+    try {
+        const categories = await database.query("SELECT * FROM categories");
+        res.send(categories.rows);
     }
-    catch (e){
-        res.sendStatus(500);
+    catch (e) {
         console.log(e);
     }
 })
 
-app.post('/categories', (req, res) => {
+app.post('/categories', async (req, res) => {
 
-    const {name} = req.body;
+    const { name } = req.body;
 
-    try{
-        if(name === "") return res.sendStatus(400);
+    try {
+        if (name === "") return res.sendStatus(400);
 
-        const categories = database.query("SELECT * FROM categories");
+        const categories = await database.query(`SELECT * FROM categories WHERE name=$1`, [name]);
 
-        const validation = categories.find(categorie => {
-            categorie.name === name;
-        })
-        if(validation) return send.sendStatus(409);
+        if (categories.rows.length !== 0) return send.sendStatus(409);
 
-        res.status(201);
+        await database.query(`INSERT INTO categories (name) VALUES ($1)`, [name]);
+
+        res.sendStatus(201);
     }
-    catch (e){
-        res.sendStatus(500);
+    catch (e) {
         console.log(e);
     }
 })
 
-app.listen(4000, () => {
+app.get('/games', async (req, res) => {
+
+    const {name} = req.query;
+
+    try {
+        const games = await database.query(`SELECT * FROM games WHERE UPPER(name) LIKE UPPER($1)`, [name + "%"]);
+        res.send(games.rows);
+    }
+    catch (e) {
+        console.log(e);
+    }
+})
+
+
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
     console.log("Servidor conectado!");
 })
