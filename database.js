2bf64a92c2fe6c9fd23261acbd1e72603d3368b0
Ana Raquel Matos <anaraquelpmatos@gmail.com>
2022-06-01 12:55:39 -0300

    second commit

--- database.js
+++ database.js
@@ -5,13 +5,16 @@ dotenv.config();
 
 const {Pool} = pg;
 const connection = new Pool({
-    connectionString: process.env.DATABASE_URL
-  });
-
-  if(process.env.MODE === "PROD"){
-    connection.ssl = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
       rejectUnauthorized: false
     }
-  }
+  });
+
+  // if(process.env.MODE === "PROD"){
+  //   connection.ssl = {
+  //     rejectUnauthorized: false
+  //   }
+  // }
 
 export default connection;
